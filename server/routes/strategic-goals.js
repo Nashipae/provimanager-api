07869777b0c15bd4dfe71ded1e699d1a23003f1d
Jose Chu <jchudb93@gmail.com>
2020-07-06 11:20:20 -0500

    refactor strategic goal

--- server/routes/strategic-goals.js
+++ server/routes/strategic-goals.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { newStrategicGoal } from "./../services/strategic-goals";
+import { StrategicGoalService } from "./../services/strategic-goals";
 
 const strategicGoalRouter = express.Router();
 
 strategicGoalRouter.post("/strategic-goal", (req, res) => {
-  newStrategicGoal(req, res);
+  StrategicGoalService.new(req, res);
 });
 
 module.exports = strategicGoalRouter;
