4d7421d6600755db8e66e9bf84bf7e5c7d0b7427
Jose Chu <jchudb93@gmail.com>
2020-07-05 22:09:56 -0500

    cors body parser

--- server/app.js
+++ server/app.js
@@ -4,13 +4,18 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser"
+
 import DBConnector from "./db";
 import { AppConfig } from "./config";
 
 const dbEngine = AppConfig.databaseEngine
 DBConnector.connect(dbEngine);
 var app = express();
-
+app.use(cors)
+app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,4 +23,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use(helmet)
 export default app;
