12a3190d429cd6fd3525973b1a51b853155699e6
Patricia Horna <patricia.horna@pucp.edu.pe>
2020-07-07 21:37:41 -0500

    strategic-goal.service fix

---
+++ server/services/strategic-goals.service.js
@@ -0,0 +1,32 @@
+import StrategicGoalModel from "../models/strategic-goal.model";
+import { checkServerError } from "./utils/error-handlers";
+
+const create = async (req, res) => {
+  const strategicGoalRecord = Object.freeze({
+    name: req.body.name
+  });
+  const strategicGoal = new StrategicGoalModel(strategicGoalRecord);
+  await strategicGoal.save(err => {
+    if (checkServerError(res, err)) return;
+  });
+
+  return res.status(200).json(strategicGoal);
+};
+
+const list = async (req, res) => {
+  const strategicGoals = StrategicGoalModel.find({});
+
+  strategicGoals
+    .exec()
+    .then(strategicGoals => {
+      res.status(200).json(strategicGoals);
+    })
+    .catch((err, docs) => {
+      if (checkServerError(res, err)) return;
+    });
+  return res;
+};
+export const StrategicGoalsService = {
+  create: create,
+  list: list
+};
